77288035ec7230efdbaa41078013dcde08ef3d2e
Venus Yu <venus0yu@gmail.com>
2021-12-07 21:33:10 -0500

    removing resume to update

--- src/navigation.js
+++ src/navigation.js
@@ -8,7 +8,7 @@ class Navigation extends React.Component {
         <div className="nav-list">
           <NavLink className="nav-item" activeClassName="active" to="/home">Home</NavLink>
           <NavLink className="nav-item" activeClassName="active" to="/skills">Skills</NavLink>
-          <a className="nav-item" href="/Venus Yu - Resume.pdf" target="_blank">Resume</a>
+          {/* <a className="nav-item" href="/Venus Yu - Resume.pdf" target="_blank">Resume</a> */}
         </div>
       </div>
     );
